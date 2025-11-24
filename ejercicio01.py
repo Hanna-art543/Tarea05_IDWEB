514dd8c1cdc5b96394e4879854c4ca62f8274859
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-24 18:24:33 -0500

    Subir ejercicio 01

---
+++ ejercicio01.py
@@ -0,0 +1,6 @@
+sabado = float(input("Ingrese la temperatura del sábado: "))
+domingo = float(input("Ingrese la temperatura del domingo: "))
+
+promedio = (sabado + domingo) / 2
+
+print("EL promedio del fin de semana es:", promedio)
