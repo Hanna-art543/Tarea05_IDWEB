320263a998b2434ee0af740263c902370d2310b2
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-24 18:48:35 -0500

    Subir ejercicio 04

---
+++ ejercicio04.py
@@ -0,0 +1,9 @@
+numero = int(input("Ingresa un número: "))
+
+if numero > 0:
+    print(f"El número {numero} es positivo.")
+elif numero == 0:
+    print(f"El número {numero} es 0.")
+
+else:
+    print (f"El número {numero} es negativo.")
