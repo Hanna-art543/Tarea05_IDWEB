06486fecc5135e90c7696088e2186f8036e4c0c8
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-24 18:32:38 -0500

    Subir ejercicio 02

---
+++ ejercicio02.py
@@ -0,0 +1,10 @@
+producto1 = float(input("Precio producto 1: "))
+producto2 = float(input("Precio producto 2: "))
+producto3 = float(input("Precio producto 3: "))
+
+subTotal = producto1 + producto2 + producto3
+
+totalPagar = subTotal * 1.18
+
+print("Subtotal:", subTotal)
+print("Monto total a pagar:", totalPagar)
